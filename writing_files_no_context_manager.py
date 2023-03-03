69023c2ea72a605e24cd3ceddfc4c215dcd7d9b7
atyuna <121321846+atyuna@users.noreply.github.com>
2023-03-03 11:49:23 -0500

    Update writing_files_no_context_manager.py

--- writing_files_no_context_manager.py
+++ writing_files_no_context_manager.py
@@ -3,13 +3,13 @@ a_string = 'Ha ha ha it is only the beginning!'
 
 # exemple 1
 
-my_f = open('/Users/anna/Desktop/txt/sample_output1.txt', 'w')
+my_f = open('/Users/txt/sample_output1.txt', 'w')
 my_f.write(my_string)
 my_f.close()
 
 # exemple 2
 
-my_f = open('/Users/anna/Desktop/txt/sample_output1.txt', 'a')
+my_f = open('/Users/txt/sample_output1.txt', 'a')   #path changed
 my_f.write(a_string + '\n')
 my_f.write(a_string + '\n')
 my_f.write(my_string + "\n")
