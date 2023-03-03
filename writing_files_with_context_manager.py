4fa309052013343e88ab05b4f1fe7d96a1f09f70
atyuna <121321846+atyuna@users.noreply.github.com>
2023-03-03 11:50:23 -0500

    Update writing_files_with_context_manager.py

--- writing_files_with_context_manager.py
+++ writing_files_with_context_manager.py
@@ -2,12 +2,12 @@ my_string = 'Once upon a time there was a girl....'
 
 # ex 1
 
-# with open('/Users/anna/Desktop/txt/sample_output2.txt', 'w') as f:
+# with open('/Users/txt/sample_output2.txt', 'w') as f:
 #     f.write(my_string)
 
 # ex 2 (appending)
 my_list = ['user1', 'user2', 'user3']
-with open('/Users/anna/Desktop/txt/sample_output2.txt', 'a') as f:
+with open('/Users/txt/sample_output2.txt', 'a') as f:
     for i in my_list:
         f.write(i + '\n')
         f.write(i + '\t')
@@ -18,12 +18,12 @@ with open('/Users/anna/Desktop/txt/sample_output2.txt', 'a') as f:
 # ex 3
 
 my_langs = ['Java','Python','Ruby','PHP']
-with open('/Users/anna/Desktop/txt/my_lang.csv','w') as l:
+with open('/Users/txt/my_lang.csv','w') as l:
     l.writelines(my_langs)
 
 # ex
 
 my_langs = ['Java','Python','Ruby','PHP']
-with open('/Users/anna/Desktop/txt/my_lang_join.csv','w') as g:
+with open('/Users/txt/my_lang_join.csv','w') as g:
     my_str_lang = '\n'.join(my_langs)
     g.write(my_str_lang)
