1e7f1fb576d2c3f80959cb5386660e64962a68be
atyuna <121321846+atyuna@users.noreply.github.com>
2023-03-03 11:48:52 -0500

    Update reading_files_with_context_manager.py

--- reading_files_with_context_manager.py
+++ reading_files_with_context_manager.py
@@ -1,7 +1,7 @@
 
 import pdb
 # sample 1
-# sample_file = '/Users/anna/Desktop/txt/Favola.txt'
+# sample_file = '/Users/txt/Favola.txt'. #path changed
 # with open(sample_file, 'r') as f:
 #     content = f.read()
 #
@@ -10,7 +10,7 @@ import pdb
 
 
 # sample 2
-countries_file = '/Users/anna/Desktop/countries.rtf'
+countries_file = '/Users/countries.rtf' #path changed
 with open(countries_file, 'r') as f:
     countries = f.readlines()
 
